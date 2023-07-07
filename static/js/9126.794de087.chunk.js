897ccf80d4fc39f4e39473f611823bf2f598afb0
VoDuy89123 <votruongduy1943@gmail.com>
2023-07-07 17:15:37 +0700

    Updates

---
+++ static/js/9126.794de087.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkdat_ve_xe=self.webpackChunkdat_ve_xe||[]).push([[9126],{9126:function(a,t,e){e.d(t,{RDM:function(){return r}});var n=e(9983);function r(a){return(0,n.w_)({tag:"svg",attr:{fill:"currentColor",viewBox:"0 0 16 16"},child:[{tag:"path",attr:{d:"M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm6.5 4.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3a.5.5 0 0 1 1 0z"}}]})(a)}}}]);
+//# sourceMappingURL=9126.794de087.chunk.js.map
